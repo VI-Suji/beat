129a8d07a60a549a6ad58f279c4841c4ca33198b
VI-Suji <sujithvi08@gmail.com>
2020-11-05 14:37:24 +0530

    updating

--- signup.php
+++ signup.php
@@ -94,7 +94,7 @@ if(isset($_POST['LOGIN']))
       <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="username">
       <p class="text-red-500 text-xs italic"></p>
     </div>
-    <div class="inline-block relative w-64">
+    <!-- <div class="inline-block relative w-64">
     <label class="block text-gray-700 text-sm font-bold mb-2" for="dept">
         Department
       </label>
@@ -118,12 +118,8 @@ if(isset($_POST['LOGIN']))
         Semester
       </label>
   <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" name="sem">
-    <option>1</option>
-    <option>2</option>
     <option>3</option>
-    <option>4</option>
     <option>5</option>
-    <option>6</option>
     <option>7</option>
     <option>8</option>
 
@@ -131,11 +127,10 @@ if(isset($_POST['LOGIN']))
   <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
   </div>
-</div>
-<br><br>
+</div> -->
 <div class="mb-2">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Batch
+        Batch(Eg: T3,R5)
       </label>
       <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="batch" name="batch" type="text" placeholder="Batch">
       <p class="text-red-500 text-xs italic"></p>
@@ -153,7 +148,6 @@ if(isset($_POST['LOGIN']))
     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
   </div>
 </div>
-<br> <br>
 
 <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="admid">
